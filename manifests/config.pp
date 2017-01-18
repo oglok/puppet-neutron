d80bbc3a2cf2319eed4747636449af9f1993f39a
Ricardo Noriega <rnoriega@redhat.com>
2017-01-18 14:00:25 +0100

    Adding more config

--- manifests/config.pp
+++ manifests/config.pp
@@ -81,6 +81,7 @@
 class neutron::config (
   $server_config                 = {},
   $api_config                    = {},
+  $bgpvpn_service_config         = {},
   $l3_agent_config               = {},
   $dhcp_agent_config             = {},
   $lbaas_agent_config            = {},
@@ -103,6 +104,7 @@ class neutron::config (
 
   validate_hash($server_config)
   validate_hash($api_config)
+  validate_hash($bgpvpn_service_config)
   validate_hash($l3_agent_config)
   validate_hash($dhcp_agent_config)
   validate_hash($lbaas_agent_config)
@@ -122,6 +124,7 @@ class neutron::config (
 
   create_resources('neutron_config', $server_config)
   create_resources('neutron_api_config', $api_config)
+  create_resources('neutron_bgpvpn_service_config', $bgpvpn_service_config)
   create_resources('neutron_l3_agent_config', $l3_agent_config)
   create_resources('neutron_dhcp_agent_config', $dhcp_agent_config)
   create_resources('neutron_lbaas_agent_config', $lbaas_agent_config)
